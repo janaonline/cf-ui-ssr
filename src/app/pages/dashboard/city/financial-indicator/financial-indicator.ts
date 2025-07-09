12ea40d2786e0882ec7d4c27fead3cde3315c9e5
Navinder Singh <navinder.singh@janaagraha.org>
2025-07-09 18:26:47 +0530

    Refactor dashboard city components and enhance financial indicator layout
    - Updated city.html to improve conditional rendering and formatting.
    - Enhanced financial-indicator.html with a new layout, including loaders and filters.
    - Added styles for chart containers and accordion components in financial-indicator.scss.
    - Refactored financial-indicator.ts to utilize reactive forms and signals for state management.
    - Improved slb.html and slb.ts for better legend display and chart data handling.
    - Modified navbar.html to replace Material icons with Bootstrap icons for consistency.
    - Updated index.html to ensure proper HTML structure and include necessary meta tags.
    - Created chart-interfaces.ts to define chart-related types and interfaces for better type safety.
    - Adjusted charts.html and charts.ts for improved chart rendering and data handling.

--- src/app/pages/dashboard/city/financial-indicator/financial-indicator.ts
+++ src/app/pages/dashboard/city/financial-indicator/financial-indicator.ts
@@ -1,12 +1,108 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal, viewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatAccordion } from '@angular/material/expansion';
+import { MaterialModule } from '../../../../material.module';
+import { ChartConfig } from '../../../../shared/components/charts/chart-interfaces';
+import { Charts } from '../../../../shared/components/charts/charts';
+import {
+  baseChartOptions,
+  DEFAULT_FONT_FAMILY,
+} from '../../../../shared/components/charts/constants';
 import { NoDataFound } from '../../../../shared/components/no-data-found/no-data-found';
+import { TabButtons } from '../../../../shared/components/tab-buttons/tab-buttons';
+import { ButtonObj } from '../../../../core/models/interfaces';
+import { PreLoader } from '../../../../shared/components/pre-loader/pre-loader';
 
 @Component({
   selector: 'app-financial-indicator',
-  imports: [NoDataFound],
+  imports: [
+    NoDataFound,
+    Charts,
+    MatAccordion,
+    MaterialModule,
+    TabButtons,
+    PreLoader,
+  ],
   templateUrl: './financial-indicator.html',
   styleUrl: './financial-indicator.scss',
 })
 export class FinancialIndicator {
-  yearsSignal = input.required<string[]>();
+  readonly disabledColor = '#e9ecef';
+  readonly primaryColor = '#1b4965';
+  readonly secondaryColor = '#62b6cb';
+  readonly accentColor = '#bee9e8';
+  readonly lineColor = '#f43f5e';
+  readonly items = [
+    { icon: 'bi bi-arrows-fullscreen', label: 'Expand' },
+    { icon: 'bi bi-download', label: 'Download' },
+    { icon: 'bi bi-share-fill', label: 'Share' },
+  ];
+  readonly buttons: ButtonObj[] = [
+    { key: 'revenue', label: 'Revenue' },
+    { key: 'ownRevenue', label: 'Own Revenue' },
+    { key: 'expenditure', label: 'Expenditure' },
+    { key: 'capex', label: 'Capital Expenditure' },
+  ];
+  legendItems: { color: string; label: string; icon: string }[] = [];
+  currentSelectedButtonKey = signal<string>('');
+
+  myForm!: FormGroup;
+  years = input.required<string[]>();
+  accordion = viewChild.required(MatAccordion);
+
+  isLoading = signal<boolean>(true);
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({ year: [this.years()[0]] });
+    this.isLoading.set(false);
+  }
+
+  // Output emitted by child to parent
+  onSelectedButtonChange(key: string): void {
+    console.log('Button key sent from child to parent:', key);
+    this.currentSelectedButtonKey.set(key);
+  }
+
+  chartData: ChartConfig = {
+    chartId: 'mixed0',
+    chartType: 'mixedChart',
+    labels: ['2020-21', '2021-22', '2022-23'],
+    data: {
+      labels: ['2020-21', '2021-22', '2022-23'],
+      datasets: [
+        {
+          type: 'line',
+          label: 'Y-o-Y Growth',
+          data: [-20, -10, 0],
+          borderWidth: 2,
+          borderColor: this.lineColor,
+          pointBackgroundColor: this.lineColor,
+          fill: false,
+          tension: 0.3,
+        },
+        {
+          type: 'bar',
+          label: 'ULB Name',
+          data: [2937, 3524, 3883],
+          backgroundColor: [this.secondaryColor],
+          borderRadius: 5,
+        },
+        {
+          type: 'bar',
+          label: 'National Avg',
+          data: [1576, 1946, 3037],
+          backgroundColor: [this.primaryColor],
+          borderRadius: 5,
+        },
+      ],
+    },
+    options: baseChartOptions(
+      DEFAULT_FONT_FAMILY,
+      true,
+      'Years',
+      'Amt in ₹ Cr'
+    ),
+  };
 }
