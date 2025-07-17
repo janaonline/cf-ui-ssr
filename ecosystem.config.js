f542db910aed537e8cda3ec01d7c4bd97bc19a5f
jeevad <jeevananthamcse@gmail.com>
2025-07-17 21:31:57 +0530

    added shell script to deploy automatically

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,22 @@ module.exports = {
     apps: [
         {
             name: 'cf-ui-ssr',
-            script: 'dist/cf-ui-ssr/server/server.mjs',
+            script: 'dist_ssr/cf-ui-ssr/server/server.mjs',
             instances: 1,
             exec_mode: 'fork',
             env: {
                 NODE_ENV: 'production',
+                PORT: 4000,
+                PM2_ENABLED: "true"
+            }
+        },
+        {
+            name: 'stag-cf-ui-ssr',
+            script: 'dist/cf-ui-ssr/server/server.mjs',
+            instances: 1,
+            exec_mode: 'fork',
+            env: {
+                NODE_ENV: 'staging',
                 PORT: 4000,
                 PM2_ENABLED: "true"
             }
---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Build Angular project
+echo "Building Angular app..."
+npm run build:prod 
+
+# Rename dist folder if needed
+BUILD_PATH=dist
+TARGET_PATH=dist_ssr
+
+echo "Backup existing build folder..."
+mv $TARGET_PATH bkp/$TARGET_PATH-`date +"%y-%m-%d"
+
+echo "Renaming new build folder..."
+# rm -rf $TARGET_PATH
+mv $BUILD_PATH $TARGET_PATH
+
+# Start the static server
+echo "Starting static server on port 4000..."
+pm2 reload ecosystem.config.js --only cf-ui-ssr || pm2 start ecosystem.config.js --only cf-ui-ssr
+echo "Static server started successfully."
