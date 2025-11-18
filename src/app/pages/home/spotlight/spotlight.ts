407ace43efc29706fc82c1ae44a64e5aea9dd30d
Kushal-MP08 <kushal.mp@janaagrha.org>
2025-11-18 10:08:22 +0530

    added homescreen annoucemnt

--- src/app/pages/home/spotlight/spotlight.ts
+++ src/app/pages/home/spotlight/spotlight.ts
@@ -3,22 +3,27 @@ import { Component } from '@angular/core';
 // import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 // import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-spotlight',
   imports: [
     // NgbCarouselModule,
     // JsonPipe,
     // CarouselModule,
-
+    MatTooltipModule,
     SlickCarouselModule,
   ],
   templateUrl: './spotlight.html',
   styleUrl: './spotlight.scss',
 })
-export class Spotlight {
-  // images = [944, 1011, 984,944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
+export class Spotlight {
+  constructor(
+    private router: Router,
+  ) {
+  }
+  // images = [944, 1011, 984,944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   // customOptions: OwlOptions = {
   //   loop: true,
   //   mouseDrag: false,
@@ -100,7 +105,9 @@ export class Spotlight {
         "downloadUrl": "https://jana-cityfinance-live.s3.ap-south-1.amazonaws.com/resource/Property_Tax_Reforms_Toolkit.pdf"
       }
     ];
-
+  goToMarketDashboard() {
+    this.router.navigate(['/municipal-data/city/comparewith']);
+  }
   slideConfig = {
     slidesToShow: 3,
     slidesToScroll: 3,
