bc84bc5b06b51080c765368f19d80bf8d70b9035
Jeeva <jeevanantham.d@janaagraha.org>
2026-04-20 23:12:38 +0530

    feat: Integrate ToStorageUrlPipe for URL handling in AfsPdfsDialog and BalancesheetIncomestatement components

--- src/app/pages/dashboard/city/balancesheet-incomestatement/balancesheet-incomestatement.ts
+++ src/app/pages/dashboard/city/balancesheet-incomestatement/balancesheet-incomestatement.ts
@@ -34,6 +34,7 @@ import { NoDataFound } from '../../../../shared/components/no-data-found/no-data
 import { TabButtons } from '../../../../shared/components/tab-buttons/tab-buttons';
 import { DashboardService } from '../../dashboard-service';
 import { CompareBy } from './compare-by/compare-by';
+import { ToStorageUrlPipe } from '../../../../core/pipes/to-storage-url.pipe';
 
 type DownloadReportElement = {
   type: string;
@@ -161,6 +162,7 @@ export class BalancesheetIncomestatement implements OnInit, OnDestroy {
     private dialog: MatDialog,
     private titleCasePipe: TitleCasePipe,
     private excelService: ExcelService,
+    private toStorageUrlPipe: ToStorageUrlPipe,
   ) { }
 
   ngOnInit(): void {
@@ -402,11 +404,8 @@ export class BalancesheetIncomestatement implements OnInit, OnDestroy {
           // console.log(res);
           if (res['data'].length == 0) this.openDialog(null, 'notFound');
           else {
-            let target_file_url =
-              environment.STORAGE_BASEURL + res['data'][0]['fileUrl'];
-            if (res['data'][0]['fileUrl'] && res['data'][0]['fileUrl'].toLowerCase().startsWith('https://')) {
-              target_file_url = res['data'][0]['fileUrl'];
-            }
+            let target_file_url = this.toStorageUrlPipe.transform(res['data'][0]['fileUrl']);
+
             const target_file_name = res['data'][0]['fileName'];
 
             if (fileType === 'pdf') window.open(target_file_url, '_blank');
