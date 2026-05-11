12750599c0b34b94f1a2aa9063305f8df3642bab
Kushal-MP08 <kushal.mp@janaagrha.org>
2026-05-11 12:17:58 +0530

    redirect old login page to home

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,13 @@ const angularApp = new AngularNodeAppEngine();
  */
 
 /**
+ * Redirect legacy FC Grant URL to home page.
+ */
+app.get('/v1/fc_grant', (_req, res) => {
+  res.redirect(301, '/home');
+});
+
+/**
  * Serve version.json without caching so the browser always gets the latest hash.
  * This route must be registered before the static middleware which sets maxAge: '1y'.
  */
